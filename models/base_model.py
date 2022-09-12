a06f20c8782bf47fcadb94b73ffb7709e0faa5b0
Hizaram <promisevector@gmail.com>
2022-09-12 19:55:37 +0100

    Made some files executable

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,28 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
 
-        from models import storage
+        # Initialize database columns
+        id = Column(String(60), nullable=False, primary_key=True)
+        created_at = Column(DateTime, nullable=False,
+                            default=datetime.utcnow())
+        updated_at = Column(DateTime, nullable=False default=created_at)
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        storage.new(self)
-        
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
@@ -32,12 +41,21 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        """Deletes current instance from storage"""
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
+
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
+
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
