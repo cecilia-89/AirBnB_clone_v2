a617040beb27c2d4852e42dd5cb2581db842f095
Hizaram <promisevector@gmail.com>
2022-09-12 13:06:18 +0100

    Corrected the name of the database for grant privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,6 +3,5 @@
 -- Shouldn't fail if either database or user already exists
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_dev_deb.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,6 +3,6 @@
 -- Shouldn't fail if either database or user already exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_deb.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
