4cb849ee49151d7462d0e7ddf30886e2d8223a55
Hizaram <promisevector@gmail.com>
2022-09-14 10:58:53 +0100

    Modified `models/review.py` to cater to sqlalchemy relationship(correction)

--- models/place.py
+++ models/place.py
@@ -2,7 +2,7 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchmey.orm import relationship
+from sqlalchemy.orm import relationship
 from os import getenv
 import models
 
