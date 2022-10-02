d6394818b47a781c484b99d33f4b061d9a032c0c
cecilia-89 <catabong89@gmail.com>
2022-10-02 19:42:24 +0100

    initial commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
-#sets up web servers for the deployment of web_static
+# Sets up a web server for deployment of web_static.
 
-sudo apt-get update
-sudo apt-get install nginx
+apt-get update
+apt-get install -y nginx
 
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-echo "Holberton School" | sudo tee -a /data/web_static/releases/test/index.html
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-sudo sed -i '47a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-
-sudo service nginx restart
+service nginx restart
