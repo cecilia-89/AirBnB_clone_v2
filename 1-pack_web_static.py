019d05f6ec601a9a3c306688808c2d25f625f2b0
cecilia-89 <catabong89@gmail.com>
2022-10-03 01:42:07 +0100

    added tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module: 1-pack_web_static"""
 
 from datetime import datetime
 from fabric.api import local
@@ -6,6 +7,7 @@ import os.path as path
 
 
 def do_pack():
+	"""archives web_static folder"""
 	date = str(datetime.now())
 	for i in [':','-','.',' ']:
 		date = date.replace(i, '')
