6c9a2d979f6f3c60d1587a77ce05a6ca6a5a8820
cecilia-89 <catabong89@gmail.com>
2022-10-03 18:02:36 +0100

    initial commit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
+"""Fabfile to generates a .tgz archive from the contents of web_static."""
 
 from time import strftime
 from fabric.api import local
