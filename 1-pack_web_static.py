61acd3cd1460401f927125a65dc5b0a824977ce5
cecilia-89 <catabong89@gmail.com>
2022-10-06 22:43:54 +0100

    added tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script generates .tgz archive of all in web_static/ using func 'do_pack'
+Module: 1-pack_web_static
 Usage: fab -f 1-pack_web_static.py do_pack
 """
 from fabric.api import local
