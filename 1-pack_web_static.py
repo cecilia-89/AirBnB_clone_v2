a03278606df71ca033d8aca57ea4137c053edd2c
cecilia-89 <catabong89@gmail.com>
2022-10-06 22:35:26 +0100

    added tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
+
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
-    
