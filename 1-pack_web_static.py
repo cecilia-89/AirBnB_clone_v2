02fe5d9476e85d36b48007420db842d018964bc2
cecilia-89 <catabong89@gmail.com>
2022-10-06 22:38:46 +0100

    added tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,8 +7,8 @@ from fabric.api import local
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
+	date = strf("%Y%M%d%H%M%S")
 	try:
-        date = strf("%Y%M%d%H%M%S")
         file = f"web_static_{date}.tgz"
         local("mkdir versions")
         local(f"tar -czvf versions/{file} web_static")
