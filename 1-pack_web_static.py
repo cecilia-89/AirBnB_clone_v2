53a059b3af86c5f85f89f311f08db56e7bc49506
cecilia-89 <catabong89@gmail.com>
2022-10-06 22:41:43 +0100

    added tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
+"""
+Fabric script generates .tgz archive of all in web_static/ using func 'do_pack'
+Usage: fab -f 1-pack_web_static.py do_pack
+All files in the folder web_static must be added to the final archive
+All archives must be stored in the folder 'versions' (create folder if none)
+Create archive "web_static_<year><month><day><hour><minute><second>.tgz"
+The function do_pack must return the archive path, else return None
+"""
 from fabric.api import local
 from time import strftime
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-	date = strf("%Y%M%d%H%M%S")
-	try:
-        file = f"web_static_{date}.tgz"
-        local("mkdir versions")
-        local(f"tar -czvf versions/{file} web_static")
-        return file
-
-    except Exception as err:
+    """generate .tgz archive of web_static/ folder"""
+    timenow = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        filename = "versions/web_static_{}.tgz".format(timenow)
+        msg = 'Packing web_static to {}'.format(filename)
+        print(msg)
+        local("tar -cvzf {} web_static/".format(filename))
+        return filename
+    except:
         return None
