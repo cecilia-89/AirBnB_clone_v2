f8223544a19c21885bba361c088ccd59d9989c23
cecilia-89 <catabong89@gmail.com>
2022-10-06 22:28:49 +0100

    added tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,13 +4,17 @@ from time import strftime
 from fabric.api import local
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    try:
-        date = strf("%Y%M%d%H%M%S")
-        file = f"web_static_{date}.tgz"
-        local("mkdir versions")
-        local(f"tar -czvf versions/{file} web_static")
-        return file
-
-    except Exception as err:
+	 """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
