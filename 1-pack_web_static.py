b8e6245768d94a9a0ad56a98e8d658e8d3683228
cecilia-89 <catabong89@gmail.com>
2022-10-06 22:57:45 +0100

    added tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,9 +10,8 @@ def do_pack():
     """creates a .tgz archive"""
     date = strf("%Y%M%d%H%M%S")
     try:
-
+		local("mkdir -p versions")
 		file = "web_static_{}.tgz".format(date)
-        local("mkdir -p versions")
         local("tar -czvf versions/{} web_static").format(file)
         return file
     except:
