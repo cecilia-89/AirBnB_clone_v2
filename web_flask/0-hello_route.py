fbe6f358f10f9a686ae65b69a620a43a83ca2a06
cecilia-89 <catabong89@gmail.com>
2022-10-14 14:15:51 +0100

    added tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
-    """returns hello HBNB on port 5000"""
+    '''returns hello HBNB on port 5000'''
     return "Hello HBNB!"
 
 
