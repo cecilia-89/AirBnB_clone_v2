d6a3a92b83aa831d69278ce547c384576cb5bdc6
cecilia-89 <catabong89@gmail.com>
2022-10-14 09:49:31 +0100

    added tasks

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_is(text):
     """returns C <text>"""
-    return f"C {text}"
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
