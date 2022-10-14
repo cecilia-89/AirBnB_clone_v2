e19e269e5bbb59b2accd90a884fffe2d8cf09eaf
cecilia-89 <catabong89@gmail.com>
2022-10-14 12:40:07 +0100

    added tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""starts a simple flask web app"""
+
 from flask import Flask
 
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
-    
     return "Hello HBNB!"
 
 
