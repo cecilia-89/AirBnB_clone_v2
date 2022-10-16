70460186a192734a12dffa18893e8a96e1304768
cecilia-89 <catabong89@gmail.com>
2022-10-16 20:27:12 +0100

    initial commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,9 +3,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/', strict_slashes=False)
-def hello_HBNB():
-    """displays 'Hello HBNB!'"""
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
