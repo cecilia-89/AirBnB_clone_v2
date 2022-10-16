a9269eb63db49ff383490b2e00dd64dc1594a3f0
cecilia-89 <catabong89@gmail.com>
2022-10-16 21:20:04 +0100

    initial commit

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,17 +8,23 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def list_states(id=""):
-    """lists all cities in alphabetical order"""
-    Storage = storage.all(State)
-    for state in Storage.values():
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
         if state.id == id:
-            return render_template("9-states.html", States=state)
-        elif id == "":
-            return render_template("9-states.html", States=Storage)
+            return render_template("9-states.html", state=state)
     return render_template("9-states.html")
+
 
 
 @app.teardown_appcontext
