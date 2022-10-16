580b9b806ee2e3c34bca69b7dcc31fd3baafa2d5
cecilia-89 <catabong89@gmail.com>
2022-10-16 01:47:45 +0100

    added tasks

---
+++ web_flask/9-states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""starts a simple flask web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route('/states/<id>', strict_slashes=False)
+@app.route('/states/', strict_slashes=False)
+def list_states():
+    """lists all cities in alphabetical order"""
+    Storage = storage.all(State).values()
+    return render_template("8-cities_by_states.html", States=Storage)
+
+
+@app.teardown_appcontext
+def tear_Down(exception):
+    """closes a db session or reload file storage"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
